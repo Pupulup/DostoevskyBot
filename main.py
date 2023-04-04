0f8f6348c8e36e74ab1e75a3453920082b7113d5
Osuuspankk1 <101213195+Pupulup@users.noreply.github.com>
2023-04-04 18:54:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
-import telegram
-from telegram import Updater, CommandHandler, MessageHandler, Filters
+import aiogram
+from aiogram import Bot, Dispatcher, executor, types
+from secret_info import API_TOKEN
 
-API_KEY = 'токен бота'
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text='Привет!')
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
 
-def echo(update, context):
-    user_text = update.message.text
-    response = generate_response(user_text)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет!\n Пришлите текст, который хотите сжать")
 
 
-def generate_response(user_text):
-    # Вставьте здесь ваш код для генерации ответа на основе текста пользователя
-    # ...
-    return 'Ваш ответ здесь'
-
-
-updater = Updater(token=API_KEY, use_context=True)
-
-# Создаем обработчики команд и текстовых сообщений
-start_handler = CommandHandler('start', start)
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-
-# Регистрируем обработчики в экземпляре Updater
-updater.dispatcher.add_handler(start_handler)
-updater.dispatcher.add_handler(echo_handler)
-
-# Запускаем бота
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
