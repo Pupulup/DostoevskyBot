acec39e2c7745eb471608ac170b76688d8ca5c6b
Osuuspankk1 <101213195+Pupulup@users.noreply.github.com>
2023-05-01 23:30:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,108 @@
 import telebot
-import json
+import openai
 import requests
 
-url = "https://api.aicloud.sbercloud.ru/public/v2/summarizator/predict"
-headers = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json'
-}
+openai.api_key = ""
 
-API_TOKEN = 'API'
+# инициализируем бота с помощью токена
+bot = telebot.TeleBot("")
 
-bot = telebot.TeleBot(API_TOKEN)
+chunk_size = 1500
+overlap_size = 100
 
+
+# ПИНГ БОТА
+@bot.message_handler(commands=['p'])
+def ping(message):
+    bot.send_message(message.chat.id, 'Работаю')
+
+
+# обработчик команды /sum
 @bot.message_handler(func=lambda message: True)
-def json_response(message):
-    text = message.text
-    data = {
-        "instances": [
-            {
-                "text": text,
-                "num_beams": 5,
-                "num_return_sequences": 3,
-                "length_penalty": 0.5
-            }
-        ]
-    }
-    print(data)
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    #print(response.status_code)
-    #print(response.json())
-    parsed_response = response.json()
-    origin_answer = parsed_response['origin']
-    best_answer = parsed_response['prediction_best']['bertscore']
-    print(best_answer)
-    print(origin_answer)
-    bot.send_message(message.chat.id, f"{best_answer}")
+def summarize_text(message):
+    # проверяем, что сообщение не является командой и не является пустым
+    if message.text is None or message.text.startswith('/'):
+        bot.send_message(message.chat.id, "Введите текст для суммаризации.")
+        return
+
+    # получаем текст из сообщения пользователя
+    text = message.text.strip()
+
+    # проверяем, что текст не пустой и проверяем, что текст не превышает максимальную длину
+    if len(text) == 0:
+        bot.send_message(message.chat.id, "Введите текст для суммаризации.")
+        return
+    elif len(text) > 5000:
+        bot.send_message(message.chat.id, "Текст превышает максимальную длину.")
+        return
+
+    # разбиваем текст на чанки
+    chunks = [text[i:i + chunk_size - overlap_size] for i in range(0, len(text), chunk_size - overlap_size)]
+
+    # обрабатываем каждый чанк с помощью OpenAI API и отправляем ответ на каждый чанк
+    summaries = []
+    for i, chunk in enumerate(chunks):
+        prompt_request = "Обобщи текст выделяя главное: " + chunk
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt_request,
+            temperature=0.7,
+            max_tokens=2000,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        summary = response.choices[0].text.strip()
+        summaries.append(summary)
+
+    # объединяем суммаризации чанков в один ответ
+    final_summary = " ".join(summaries)
+    final_summary = final_summary.split('.', 1)[-1].strip()
+    bot.send_message(message.chat.id, final_summary)
 
 
+@bot.message_handler(content_types=['doc'])
+def summarize_document(message):
+
+    # получаем информацию о файле
+    file_info = bot.get_file(message.document.file_id)
+    # скачиваем файл
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(bot.token, file_info.file_path))
+    # декодируем содержимое файла из байтов в текст
+    text = file.content.decode('utf-8')
+
+    # проверяем, что текст не пустой и проверяем, что текст не превышает максимальную длину
+    if not text:
+        bot.send_message(message.chat.id, "Файл пустой.")
+        return
+
+    # разбиваем текст на чанки
+    chunks = [text[i:i + chunk_size - overlap_size] for i in range(0, len(text), chunk_size - overlap_size)]
+
+    # обрабатываем каждый чанк с помощью OpenAI API
+    summaries = []
+    for i, chunk in enumerate(chunks):
+        prompt_request = "Обобщи текст используя цитаты из оригинального текста: " + chunk
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt_request,
+            temperature=0.7,
+            max_tokens=2000,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        summary = response.choices[0].text.strip()
+        summaries.append(summary)
+
+    # объединяем суммаризации чанков в один ответ
+    final_summary = " ".join(summaries)
+    bot.send_message(message.chat.id, final_summary)
 
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
-
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-"""@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, json_response())"""
-
-
-bot.infinity_polling()
+# запускаем бота
+bot.polling()
